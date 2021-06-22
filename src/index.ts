74d172bc73b1a615623875d1283310a471c53cdf
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2021-06-22 15:25:56 +0200

    set-up apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
 import express from "express";
-import { listings } from "./listings";
+import { resolvers, typeDefs } from "./graphql";
+import { ApolloServer } from "apollo-server-express";
 
 const app = express();
 const port = 9000;
 
-app.use(express.json());
-
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-});
+const server = new ApolloServer({ typeDefs, resolvers }) as any;
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 console.log(`[app]: http://localhost:${port}`);
