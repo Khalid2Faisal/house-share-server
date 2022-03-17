d6b8a1e40f6252889cf57b203ef52f6dead7ea40
Khalid Faisal <Khalid.Faisal.7070@gmail.com>
2022-03-17 11:49:12 +0200

    prepare for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import express, { Application } from "express";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 import { resolvers, typeDefs } from "./graphql";
 import { ApolloServer } from "apollo-server-express";
 import { connectDatabase } from "./database";
@@ -12,6 +12,9 @@ const mount = async (app: Application) => {
 
   app.use(express.json({ limit: "2mb" }));
   app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(compression());
+  app.use(express.static(`${__dirname}/client`));
+  app.get("/*", (_req, res) => res.sendFile(`${__dirname}/client/index.html`));
 
   const server = new ApolloServer({
     typeDefs,
